580adf4703e07347ab42156109e1db18474bbe63
Wuchan33 <mp.suchan@gmail.com>
2026-09-03 22:51:22 +0200

    Initial commit: Spring Boot project skeleton with Postgres configuration

---
+++ src/main/java/com/wuchan33/workout_progress_tracker/WorkoutProgressTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.wuchan33.workout_progress_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutProgressTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutProgressTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuchan33/workout_progress_tracker/WorkoutProgressTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuchan33.workout_progress_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutProgressTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
